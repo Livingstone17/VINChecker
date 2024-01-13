aa7b68c3409188cccba0c49d055ca763a0c92597
Livingstone17 <peterson.omobolaji@gmail.com>
2024-01-13 22:40:01 +0100

    Added loader animation

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+var loader = document.querySelector('.pl');
 
+const  displayLoading = () => {
+  loader.style.display = 'block';
+  setTimeout(() => {
+    loader.style.display = 'none';
+  }, 2000);
+}
+
+const hideLoading = () => {
+  loader.style.display = 'none';
+}
 
 const searchVIN = () => {
 var input = document.getElementById('vinNum').value;
@@ -8,7 +19,7 @@ var input = document.getElementById('vinNum').value;
     console.log(vin);
     
     var url = 'https://api.api-ninjas.com/v1/vinlookup?vin='+vin;
-    
+    displayLoading();
     fetch(url, {
       method: 'GET',
       headers: {
@@ -24,15 +35,16 @@ var input = document.getElementById('vinNum').value;
       })
       .then(result => {
         console.log(result);
-        var infoSec = document.querySelector('.result-section');
-        infoSec.style.display = 'block';
-        document.getElementById('country').innerHTML = result.country
-        document.getElementById('manufacturer').innerHTML = result.manufacturer;
-        document.getElementById('region').innerHTML = result.region
-        document.getElementById('vds').innerHTML = result.vds
-        document.getElementById('vis').innerHTML = result.vis
-        document.getElementById('wmi').innerHTML = result.wmi
-        document.getElementById('year').innerHTML = result.years
+        hideLoading();
+          var infoSec = document.querySelector('.result-section');
+          infoSec.style.display = 'block';
+          document.getElementById('country').innerHTML = result.country
+          document.getElementById('manufacturer').innerHTML = result.manufacturer;
+          document.getElementById('region').innerHTML = result.region
+          document.getElementById('vds').innerHTML = result.vds
+          document.getElementById('vis').innerHTML = result.vis
+          document.getElementById('wmi').innerHTML = result.wmi
+          document.getElementById('year').innerHTML = result.years
       })
       .catch(error => {
         console.error('Error:', error);
